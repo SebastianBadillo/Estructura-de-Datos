ce560819e5869058db0b555ab30f96e5e75d2424
Sebastian <dbadillito611@gmail.com>
2023-05-14 20:23:17 -0500

    modified

--- nodos.py
+++ nodos.py
@@ -1,39 +1,85 @@
 class Nodo:
     # Constructor
-    def __init__(self, valor):
+    def __init__(self, valor=None):
         self.valor = valor
         self.siguiente = None
 
-    # Método Ver el valor
-    def devolver_valor(self):
-        print(self.valor)
 
-    # Método asignar siguiente
-    def asignar_siguiente(self, nodo):
-        self.siguiente = nodo # Aqui se genera el 
+class List:
+    def __init__(self):
+        self.inicio = None
+        self.size = 0
+
+    def add(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if (self.inicio == None):
+            self.inicio = nuevo_nodo
+        else:
+            nodo_iterador = self.inicio
+            while nodo_iterador.siguiente != None:
+                nodo_iterador = nodo_iterador.siguiente
+            nodo_iterador.siguiente = nuevo_nodo
+        self.size += 1
     
-    #Me todo cambiar valor al nodo
-    def cambiar_valor(self, nuevo_valor):
-        self.valor = nuevo_valor
+    def delete(self, value):
+        nodo_actual = self.inicio
+        nodo_anterior = None
+        key = False
+        while nodo_actual != None:
+            if nodo_actual.valor == value:
+                key = True
+                if nodo_anterior == None:
+                    self.inicio = nodo_actual.siguiente
+                else:
+                    nodo_anterior.siguiente = nodo_actual.siguiente
+                    nodo_actual = nodo_actual.siguiente
+            else:
+                nodo_anterior = nodo_actual
+                nodo_actual = nodo_actual.siguiente
+        if key == False:
+            return 'The element is not in the list'
+        else: 
+            self.size -=1   
+ 
     
-Nodo1 = Nodo('A')
-Nodo2 = Nodo('C')
-#print(Nodo1.siguiente)  
-
-# Primera forma
-Nodo1.devolver_valor()
-
-# Segunda forma, apartir del metodo indicando el objeto
-Nodo.devolver_valor(Nodo1)
-
-Nodo1.cambiar_valor('B')
-
-Nodo1.devolver_valor()
-
-Nodo1.asignar_siguiente(Nodo2)
-print(Nodo1)
-print(Nodo1.siguiente)
-print(Nodo2)
-
-
+    def find_value(self, value):
+        nodo_iterador = self.inicio
+        indice = 0
+        key = False
+        for x in range(0, self.size):
+            if nodo_iterador.valor == value:
+                key = True
+                return indice
+            else:
+                nodo_iterador = nodo_iterador.siguiente
+            indice +=1
+        if(key != True):
+            return 'sorry, element is not in the list'
+        
+    def find_by_index(self,index):
+        nodo_iterador = self.inicio
+        if index >= self.size :
+            return 'index out of bounds'
+        else:
+            for x in range(0, index):
+                nodo_iterador = nodo_iterador.siguiente  
+            return nodo_iterador.valor
     
+    def add_first(self, value):
+        nuevo_nodo = Nodo(value)
+        nuevo_nodo.siguiente = self.inicio
+        self.inicio = nuevo_nodo
+    
+    def __str__(self):
+        if self.inicio == None:
+            return '[]'
+        else:
+            elementos = []
+            nodo_iterador = self.inicio
+            while nodo_iterador != None:
+                elementos.append(str(nodo_iterador.valor))# El str es necesario para poder usar el metodo join mas adelante
+                nodo_iterador = nodo_iterador.siguiente
+            return '[' + ', '.join(elementos) + ']'
+            
+       
+        
